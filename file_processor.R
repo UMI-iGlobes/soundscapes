16b74ae1eb94708503c0663cd775ea5ecc5116a5
coltonflowers1 <coltonflowers@email.arizona.edu>
2020-01-24 12:53:40 -0700

    added file_processor.R, along with download/imports for sound processing packages.

---
+++ file_processor.R
@@ -0,0 +1,10 @@
+# Title     : file_processor
+# Objective : Read WAV files from harddrive and compute features for analysis.
+# Created by: Biosphere
+# Created on: 1/24/2020
+
+#install sound processing packages if not already present
+#install.packages(c("signal","tuneR","seewave"))
+#install.packages(c("rpanel","rgl"))
+library(tuneR)
+library(seewave)
