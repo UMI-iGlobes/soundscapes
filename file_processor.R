31fb2fc7ccb344b6b6ceb0913f7ef59a7ff9b062
coltonflowers1 <coltonflowers@email.arizona.edu>
2020-01-24 14:18:47 -0700

    added code to get wav file name and laid out skeleton of for loop to process each file.

--- file_processor.R
+++ file_processor.R
@@ -8,3 +8,34 @@
 #install.packages(c("rpanel","rgl"))
 library(tuneR)
 library(seewave)
+
+##creates class corresponding to a sound file from data library
+Soundfile <- function(site=NA,date=NA,sound=NA)
+{
+
+        me <- list(
+                site = site,
+                date = date,
+                sound = sound
+       )
+
+        ## Set the name for the class
+        class(me) <- append(class(me),"Soundfile")
+        return(me)
+}
+##provide path to data_library below:
+data_library_path <- "D:\\data_library"
+##get all wav file names
+file.names <-list.files(data_library_path,recursive=TRUE, pattern = "wav$")
+##initalize vector to store Soundfile objects
+sound_files <- rep(NA,length(file.names))
+for (i in 1:length(file.names))
+  {
+    site = unlist(strsplit(file.names[i], "/"))[1]
+    date = unlist(strsplit(file.names[i], "/"))[2]
+    print(site)
+    print(date)
+    #sound = readWave(paste(data_library_path,file.names[i],sep='/'))
+    #sound_files[i] <- Soundfile(site,date,sound)
+}
+
