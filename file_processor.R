e67e05aca94c743b5cd29f71d66db174ac7867b0
coltonflowers1 <coltonflowers@email.arizona.edu>
2020-01-31 13:21:45 -0700

    date issue fixed; error handling for pre-processing created; need to add functionality to split a wav file into several smaller wav files and then pre-process them.

--- file_processor.R
+++ file_processor.R
@@ -2,7 +2,9 @@
 # Objective : Read WAV files from harddrive and compute features for analysis.
 # Created by: Biosphere
 # Created on: 1/24/2020
-
+CHECKMARK<-50
+WRITE_TO_FILE_PATH<-"D:\\data_library\\index_data.csv"
+DATA_LIBRARY_PATH<- "D:\\data_library"
 source("utils.R")
 #install sound processing packages if not already present
 #install.packages(c("signal","tuneR","seewave"))
@@ -12,15 +14,15 @@ library(tuneR)
 library(seewave)
 library(soundecology)
 ##provide path to data_library below:
-data_library_path <- "D:\\data_library"
+
 ##get all wav file names
-file.paths <-list.files(data_library_path,recursive=TRUE, pattern = "wav$")
+file.paths <-list.files(DATA_LIBRARY_PATH,recursive=TRUE, pattern = "wav$")
 file.names<-sapply(file.paths,function(path) {return(unlist(strsplit(path, "/"))[3])},USE.NAMES=FALSE)
 file.namedata = songmeter(file.names)
 #print(file.namedata)
 ##initalize data.frame to store indices.
 index_data <- data.frame(Site=character(),
-                 Date<-as.Date(character(),format="%y/%m/%d"),
+                 Date<-as.Date(character(),format="%Y/%m/%d"),
                  Time<-character(),
                  bioacoustic<-double(),
                  amplitude<-double(),
@@ -34,7 +36,8 @@ index_data <- data.frame(Site=character(),
                  ndsi<-double(),
                  stringsAsFactors=FALSE)
 for (i in 1:length(file.paths))
-  { year <- file.namedata[i,"year"]
+  { tryCatch({
+    year <- file.namedata[i,"year"]
     month <- file.namedata[i,"month"]
     day <- file.namedata[i,"day"]
     hour <- file.namedata[i,"hour"]
@@ -42,12 +45,26 @@ for (i in 1:length(file.paths))
     sec <- file.namedata[i,"sec"]
 
     site <- unlist(strsplit(file.paths[i], "/"))[1]
-    date <- paste(day,month,year,sep="-")
+    date <- paste(year,month,day,sep="/")
     time <- paste(hour,min,sec,sep=":")
 
-    #indices <- alpha_indices(file.paths[i])
-    indices <- rep(1,10)
+    indices <- alpha_indices(file.paths[i])
     index_data[i,] <- c(site,date,time,indices)
-    #sound = readWave(paste(data_library_path,file.names[i],sep='/'))
-    #sound_files[i] <- Soundfile(site,date,sound)
+    if (i %% 50 == 0) {
+    write.csv(index_data, file = WRITE_TO_FILE_PATH, row.names=FALSE)
+    print(paste("CHECKMARKED @ ",i,"/",length(file.paths)))
+    }
+    }
+    , error=function(e){
+      message("Caught an error at iteration: ",i," site:",site," date:",date," time:",time)
+      print(e)
+      write_to_log_file(e,logger_Level="ERR")
+    }
+    , warning = function(w){
+      message('Caught a warning at iteration: ',i," site:",site," date:",date," time:",time)
+      print(w)
+      write_to_log_file(w, logger_level = "WARN")
+    }
+)
 }
+write.csv(index_data, file = WRITE_TO_FILE_PATH, row.names=FALSE)
